34d9f43c32067963424af766c9b693b3c107a13a
leoantony72 <leoantony102@gmail.com>
2022-03-04 20:20:29 -0800

    🚀Image-Password

---
+++ password.py
@@ -0,0 +1,17 @@
+from hashlib import sha256
+
+a_string = 'this string holds important and private information'
+
+
+image_file = input("Enter file location :")
+
+def image_hash(image_file):
+    try:
+        with open(image_file,"rb") as f:
+            bytes = f.read() # read entire file as bytes
+            readable_hash = sha256(bytes).hexdigest();
+            print(readable_hash)
+    except FileNotFoundError:
+        print("File Not Found🤦")            
+
+image_hash(image_file)
